0453ddec6033d050d74b155531ea126e0ea21a82
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-08-14 04:30:57 +0000

    [start]2-1

---
+++ app/models/meshitterro.rb
@@ -0,0 +1,2 @@
+class Meshitterro < ApplicationRecord
+end
---
+++ db/migrate/20230814040538_create_meshitterros.rb
@@ -0,0 +1,8 @@
+class CreateMeshitterros < ActiveRecord::Migration[6.1]
+  def change
+    create_table :meshitterros do |t|
+
+      t.timestamps
+    end
+  end
+end
