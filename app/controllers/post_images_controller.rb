848fb026b816a1e0ef7a205f12c0695954c84ad5
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-08-17 03:30:50 +0000

    [Add]ページネーション

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,12 +6,15 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+     redirect_to post_images_path
+    else 
+      render :new
+    end
   end
 
   def index
-    @post_image = PostImage.all
+    @post_image = PostImage.page(params[:page])
   end
 
   def show
@@ -30,5 +33,4 @@ class PostImagesController < ApplicationController
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-
 end
